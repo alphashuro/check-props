f721b848c323e5f5208eb1b1b77395e296ff13af
Alpha Shuro <alphashuro@gmail.com>
2016-07-21 10:40:41 +0200

    added some missing docs

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ function checkKey(object, key, type) {
  * checks if keys specified in `props` exist in `object`
  * also checks if the values of props[key] are the type of the prop in object if props[key] is not null
  * an example of a props object is { foo: 'string', bar: null }
+ * @params {object} object - the object to check
+ * @params {object}  props  - object with the propertiess to look for
  */
 function checkProps(object, props) {
 	const keys = Object.keys(props);
